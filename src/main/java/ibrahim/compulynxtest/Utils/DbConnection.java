cc051d3d58693fa2bcacaa54075d36812293a681
Rubyroy12 <ibrakipz7@gmail.com>
2025-01-20 23:50:33 +0300

    final changes

--- src/main/java/ibrahim/compulynxtest/Utils/DbConnection.java
+++ src/main/java/ibrahim/compulynxtest/Utils/DbConnection.java
@@ -6,10 +6,13 @@ import java.sql.SQLException;
 
 public class DbConnection {
 
-    public static Connection getDBConnection() throws SQLException {
-        String url = "jdbc:mariadb://localhost:3306/compulynx";
-        String user = "ibrahim";
-        String password = "kali";
-        return DriverManager.getConnection(url, user, password);
-    }
+
+
+        public static Connection getDBConnection() throws SQLException {
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=compulynx;encrypt=true;trustServerCertificate=true";
+            String user = "ibrahim";
+            String password = "kali";
+
+            return DriverManager.getConnection(url, user, password);
+        }
 }
